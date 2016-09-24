971715d54ed920523766e11ac96a62ec8b52f264
cesar mesquita <cesar mesquita>
2016-09-24 22:29:58 +0000

    11

--- hello_django/hello/settings.py
+++ hello_django/hello/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'hello.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dboo',
+        'USER': 'dbuser',
+        'PASSWORD': 'manager1',
+        'HOST': '192.168.47.253',
+        'PORT': '3306',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
